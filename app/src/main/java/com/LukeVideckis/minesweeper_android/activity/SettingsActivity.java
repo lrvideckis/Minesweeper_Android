6c8226662ccf9a836648e1ea6ee24c36624eaf64
lrvideckis <lukevideckis@gmail.com>
2022-03-04 01:35:36 -0800

    got saving settings to persistent android storage to work

--- app/src/main/java/com/LukeVideckis/minesweeper_android/activity/SettingsActivity.java
+++ app/src/main/java/com/LukeVideckis/minesweeper_android/activity/SettingsActivity.java
@@ -1,16 +1,39 @@
 package com.LukeVideckis.minesweeper_android.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 
 import com.LukeVideckis.minesweeper_android.R;
 
 import java.util.Objects;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    public static final String
+            TOGGLE_FLAGS_SETTING = "toggleFlagsSetting",
+            GENERATE_GAMES_WITH_8_SETTING = "hasAn8Setting";
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (buttonView.getId() == R.id.defaultSetFlags) {
+            SwitchCompat toggleFlagsSetting = findViewById(R.id.defaultSetFlags);
+            editor.putBoolean(TOGGLE_FLAGS_SETTING, isChecked);
+            toggleFlagsSetting.setChecked(isChecked);
+        } else if (buttonView.getId() == R.id.isGuaranteed8) {
+            SwitchCompat toggleGamesWith8Setting = findViewById(R.id.isGuaranteed8);
+            editor.putBoolean(GENERATE_GAMES_WITH_8_SETTING, isChecked);
+            toggleGamesWith8Setting.setChecked(isChecked);
+        }
+        editor.apply();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +43,17 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        SwitchCompat toggleFlagsSetting = findViewById(R.id.defaultSetFlags);
+        SwitchCompat toggleGamesWith8Setting = findViewById(R.id.isGuaranteed8);
+
+        sharedPreferences = getSharedPreferences(StartScreenActivity.MY_PREFERENCES, Context.MODE_PRIVATE);
+
+        toggleFlagsSetting.setChecked(sharedPreferences.getBoolean(TOGGLE_FLAGS_SETTING, false));
+        toggleGamesWith8Setting.setChecked(sharedPreferences.getBoolean(GENERATE_GAMES_WITH_8_SETTING, false));
+
+        toggleFlagsSetting.setOnCheckedChangeListener(this);
+        toggleGamesWith8Setting.setOnCheckedChangeListener(this);
     }
 
     @Override
