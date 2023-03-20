360ababef31eafab8a89a406e84160bad02ccf46
Luke Videckis <lukevideckis@gmail.com>
2023-03-20 17:53:23 -0500

    adding progress

--- app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/minesweeperHelpers/CheckAllDeducibleIsMarked.java
+++ app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/minesweeperHelpers/CheckAllDeducibleIsMarked.java
@@ -8,11 +8,28 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CheckAllDeducibleIsMarked {
 
-    public static boolean checkAllDeducibleStuffIsMarked(Board<TileWithProbability> board) {
-        for(int i = 0; i < board.getRows(); i++) {
-            for(int j = 0; j < board.getCols(); j++) {
-                if(board[i][j].)
+    public static boolean checkAllDeducibleStuffIsMarked(Board<TileWithProbability> boardFromSolver, Board<Tile> boardWithFlags) {
+        const int ROWS = boardFromSolver.getRows();
+        const int COLS = boardFromSolver.getCols();
+        if(rows != boardWithFlags.getRows() || cols != boardWithFlags.getCols()) {
+            throw new Exception("either rows or cols does not match.");
+        }
+        for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+                if(boardFromSolver[i][j].isVisible) {
+                    continue;
+                }
+                if(boardFromSolver[i][j].mineProbability.equals(0)) {
+                    // Any deducible free left un-tapped means the user hasn't correctly marked all
+                    // deducible stuff, as they should have tapped this deducible free.
+                    return false;
+                }
+                if(boardFromSolver[i][j].mineProbability.equals(1) && boardWithFlags.state == TileState.NOT_FLAGGED) {
+
+                }
+
             }
         }
+        return true;
     }
 }
