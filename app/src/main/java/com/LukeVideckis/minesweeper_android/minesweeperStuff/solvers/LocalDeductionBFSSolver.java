f31dc6dae1d5330b9e5ddb359de5a4fdc7ef69c7
Luke Videckis <lukevideckis@gmail.com>
2023-05-22 14:17:09 -0500

    now create solvable board works again

--- app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/LocalDeductionBFSSolver.java
+++ app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/LocalDeductionBFSSolver.java
@@ -26,10 +26,70 @@ public class LocalDeductionBFSSolver implements SolverNothingToLogistics {
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 tmpBoard[i][j] = new TileWithLogistics();
+                tmpBoard[i][j].set(board.getCell(i, j));
             }
         }
         Board<TileWithLogistics> boardWithLogistics = new Board<>(tmpBoard, board.getMines());
 
+        boolean foundNewStuff = true;
+        while(foundNewStuff) {
+            foundNewStuff = false;
+            for (int i = 0; i < boardWithLogistics.getRows(); ++i) {
+                for (int j = 0; j < boardWithLogistics.getCols(); ++j) {
+                    TileNoFlagsForSolver cell = boardWithLogistics.getCell(i, j);
+                    if (!cell.isVisible) {
+                        continue;
+                    }
+                    ArrayList<TileWithLogistics> adjCells = boardWithLogistics.getAdjacentCells(i, j);
+                    int cntAdjacentMines = 0, cntAdjacentFrees = 0, cntTotalAdjacentCells = 0;
+                    for (TileWithLogistics adjTile : adjCells) {
+                        if (adjTile.isVisible) {
+                            continue;
+                        }
+                        ++cntTotalAdjacentCells;
+                        if (adjTile.logic == LogisticState.MINE) {
+                            ++cntAdjacentMines;
+                        }
+                        if (adjTile.logic == LogisticState.FREE) {
+                            ++cntAdjacentFrees;
+                        }
+                    }
+                    if (cntTotalAdjacentCells == 0) {
+                        continue;
+                    }
+                    if (cntAdjacentMines == cell.numberSurroundingMines) {
+                        //anything that's not a mine is free
+                        for (TileWithLogistics adjTile : adjCells) {
+                            if (adjTile.isVisible) {
+                                continue;
+                            }
+                            if (adjTile.logic == LogisticState.MINE) {
+                                continue;
+                            }
+                            if (adjTile.logic != LogisticState.FREE) {
+                                foundNewStuff = true;
+                                adjTile.logic = LogisticState.FREE;
+                            }
+                        }
+                    }
+                    if (cntTotalAdjacentCells - cntAdjacentFrees == cell.numberSurroundingMines) {
+                        //anything that's not free is a mine
+                        for (TileWithLogistics adjTile : adjCells) {
+                            if (adjTile.isVisible) {
+                                continue;
+                            }
+                            if (adjTile.logic == LogisticState.FREE) {
+                                continue;
+                            }
+                            if (adjTile.logic != LogisticState.MINE) {
+                                foundNewStuff = true;
+                                adjTile.logic = LogisticState.MINE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return boardWithLogistics;
     }
 }
