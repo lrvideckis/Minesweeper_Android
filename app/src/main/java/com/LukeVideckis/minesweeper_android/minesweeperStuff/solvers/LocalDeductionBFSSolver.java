f377189dd9f33aa5466ed96cbfcb5386b48eec1d
Luke Videckis <lukevideckis@gmail.com>
2023-05-24 13:58:36 -0500

    move all initializations to function

--- app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/LocalDeductionBFSSolver.java
+++ app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/LocalDeductionBFSSolver.java
@@ -2,13 +2,12 @@ package com.LukeVideckis.minesweeper_android.minesweeperStuff.solvers;
 
 import com.LukeVideckis.minesweeper_android.minesweeperStuff.Board;
 import com.LukeVideckis.minesweeper_android.minesweeperStuff.solvers.interfaces.SolverNothingToLogistics;
-import com.LukeVideckis.minesweeper_android.minesweeperStuff.tiles.LogisticState;
 import com.LukeVideckis.minesweeper_android.minesweeperStuff.tiles.TileNoFlagsForSolver;
 import com.LukeVideckis.minesweeper_android.minesweeperStuff.tiles.TileWithLogistics;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.TreeMap;
 
@@ -26,35 +25,37 @@ public class LocalDeductionBFSSolver implements SolverNothingToLogistics {
             throw new Exception("array bounds don't match");
         }
 
-        Queue<bfsState> q = getInitialQueue(board);
-
-        //always allocate new board to avoid any potential issues with shallow copies between solver runs
-        TileWithLogistics[][] tmpBoard = new TileWithLogistics[rows][cols];
+        Queue<bfsState> q = new LinkedList<>();
+        List<List<List<bfsState>>> gridLocationToStates = new ArrayList<>(rows);
         List<List<TreeMap<Integer, bfsValue>>> stateToValue = new ArrayList<>(rows);
-        for (int i = 0; i < rows; ++i) {
-            stateToValue.add(new ArrayList<>(cols));
-            for (int j = 0; j < cols; ++j) {
-                stateToValue.get(i).add(new TreeMap<>());
-                tmpBoard[i][j] = new TileWithLogistics();
-                tmpBoard[i][j].set(board.getCell(i, j));
-            }
-        }
-        Board<TileWithLogistics> boardWithLogistics = new Board<>(tmpBoard, board.getMines());
+        Board<TileWithLogistics> boardWithLogistics = initializeStructures(board, q, gridLocationToStates, stateToValue);
 
-        while(!q.isEmpty()) {
-            bfsState state = q.remove();
+        //TODO: a-star style approach when destination square is given
+        while (!q.isEmpty()) {
+            bfsState currState = q.remove();
+            int sizeSubset = Integer.bitCount(currState.subsetSurroundingSquares);
 
-
-            //Integer.bitCount(i);
         }
 
         return boardWithLogistics;
     }
 
-    private Queue<bfsState> getInitialQueue(Board<TileNoFlagsForSolver> board) throws Exception {
-        Queue<bfsState> q = new LinkedList<>();
-        for (int i = 0; i < board.getRows(); ++i) {
-            for (int j = 0; j < board.getCols(); ++j) {
+    private Board<TileWithLogistics> initializeStructures(
+            Board<TileNoFlagsForSolver> board,
+            Queue<bfsState> q,
+            List<List<List<bfsState>>> gridLocationToStates,
+            List<List<TreeMap<Integer, bfsValue>>> stateToValue) throws Exception {
+        final int rows = board.getRows();
+        final int cols = board.getCols();
+        TileWithLogistics[][] tmpBoard = new TileWithLogistics[rows][cols];
+        for (int i = 0; i < rows; ++i) {
+            gridLocationToStates.add(new ArrayList<>(cols));
+            stateToValue.add(new ArrayList<>(cols));
+            for (int j = 0; j < cols; ++j) {
+                gridLocationToStates.get(i).add(new ArrayList<>());
+                stateToValue.get(i).add(new TreeMap<>());
+                tmpBoard[i][j] = new TileWithLogistics();
+                tmpBoard[i][j].set(board.getCell(i, j));
                 if (!board.getCell(i, j).isVisible) {
                     continue;
                 }
@@ -68,36 +69,58 @@ public class LocalDeductionBFSSolver implements SolverNothingToLogistics {
                     if (board.getCell(adjI, adjJ).isVisible) {
                         continue;
                     }
-                    dirMask += (1<<dir);
+                    dirMask += (1 << dir);
                 }
-                if(dirMask > 0) {
+                if (dirMask > 0) {
                     int numMines = board.getCell(i, j).numberSurroundingMines;
-                    q.add(new bfsState(i, j, dirMask));
+                    if (numMines == 0) {
+                        throw new Exception("visible squares with at least 1 non-visible neighbor should have non-zero number of mines.");
+                    }
+                    bfsState currState = new bfsState(i, j, dirMask);
+                    q.add(currState);
+                    for (int dir = 0; dir < 8; dir++) {
+                        if (((dirMask >> dir) & 1) == 0) {
+                            continue;
+                        }
+                        final int adjI = i + Board.deltas[dir][0];
+                        final int adjJ = j + Board.deltas[dir][1];
+                        gridLocationToStates.get(adjI).get(adjJ).add(currState);
+                    }
                 }
             }
         }
-        return q;
+
+        //always allocate new board to avoid any potential issues with shallow copies between solver runs
+        return new Board<>(tmpBoard, board.getMines());
     }
 
     private class bfsState {
         //represents index of center-cell
-        private int centerI, centerJ;
+        private final int centerI;
+        private final int centerJ;
         //number in range [0, 2^8)
         //012
         //3 4
         //567
         //each on-bit means that that relative tile is in the subset
-        private int subsetSurroundingSquares;
+        private final int subsetSurroundingSquares;
 
         public bfsState(int centerI, int centerJ, int subsetSurroundingSquares) {
             this.centerI = centerI;
             this.centerJ = centerJ;
             this.subsetSurroundingSquares = subsetSurroundingSquares;
         }
+
+        public boolean intersects(bfsState otherState) {
+            return false;//TODO
+        }
     }
+
     private class bfsValue {
-        private int minNumMines, maxNumMines;
+        private final int minNumMines;
+        private final int maxNumMines;
         private bfsState prevState1 = null, prevState2 = null, prevState3 = null;
+
         public bfsValue(int minNumMines, int maxNumMines, bfsState prevState1, bfsState prevState2, bfsState prevState3) {
             this.minNumMines = minNumMines;
             this.maxNumMines = maxNumMines;
