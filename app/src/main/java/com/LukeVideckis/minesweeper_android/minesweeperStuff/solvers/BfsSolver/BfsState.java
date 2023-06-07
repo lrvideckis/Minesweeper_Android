6e87a5cf8c9da546ec32f045dbcee5f3a628ac4e
Luke Videckis <lukevideckis@gmail.com>
2023-06-07 11:42:49 -0500

    implement is subset and add test

--- app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/BfsSolver/BfsState.java
+++ app/src/main/java/com/LukeVideckis/minesweeper_android/minesweeperStuff/solvers/BfsSolver/BfsState.java
@@ -1,5 +1,7 @@
 package com.LukeVideckis.minesweeper_android.minesweeperStuff.solvers.BfsSolver;
 
+import com.LukeVideckis.minesweeper_android.minesweeperStuff.Board;
+
 public class BfsState {
     //represents location of center-cell
     final int centerI, centerJ;
@@ -18,9 +20,34 @@ public class BfsState {
         this.centerIsVisible = centerIsVisible;
     }
 
+    //TODO: optimize to O(1) instead of O(8)?
+    private boolean containsCell(int cellI, int cellJ) {
+        for (int dir = 0; dir < 8; dir++) {
+            if (((this.subsetSurroundingSquares >> dir) & 1) == 0) {
+                continue;
+            }
+            final int adjI = this.centerI + Board.deltas[dir][0];
+            final int adjJ = this.centerJ + Board.deltas[dir][1];
+            if (adjI == cellI && adjJ == cellJ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     //returns true iff otherState is a subset of me
     public boolean isSubsetOfMe(BfsState otherState) {
-        return false;//TODO
+        for (int dir = 0; dir < 8; dir++) {
+            if (((otherState.subsetSurroundingSquares >> dir) & 1) == 0) {
+                continue;
+            }
+            final int adjI = otherState.centerI + Board.deltas[dir][0];
+            final int adjJ = otherState.centerJ + Board.deltas[dir][1];
+            if(!this.containsCell(adjI, adjJ)) {
+                return false;
+            }
+        }
+        return true;
     }
     //returns the set difference: a new BfsState, where each square in it's subset is in me and not in otherState
     public BfsState inMeNotInThem(BfsState otherState) {
--- app/src/test/java/com/LukeVideckis/minesweeper_android/minesweeper_tests/BfsStateTests.java
+++ app/src/test/java/com/LukeVideckis/minesweeper_android/minesweeper_tests/BfsStateTests.java
@@ -3,17 +3,51 @@ import com.LukeVideckis.minesweeper_android.minesweeperStuff.solvers.BfsSolver.B
 
 import org.junit.Test;
 
+//012
+//3 4
+//567
+//public BfsState(int centerI, int centerJ, int subsetSurroundingSquares, boolean centerIsVisible) {
 public class BfsStateTests {
     @Test
     public void testBfsStateIsSubset() throws Exception {
-
-        //012
-        //3 4
-        //567
-    //public BfsState(int centerI, int centerJ, int subsetSurroundingSquares, boolean centerIsVisible) {
         BfsState state1 = new BfsState(0, 0, 2, false);
-        BfsState state2 = new BfsState(1, 0, 1, false);
+        BfsState state2 = new BfsState(0, 1, 1, false);
         if(!state1.isSubsetOfMe(state2)) {
+            throw new Exception("failed");
+        }
+        if(!state2.isSubsetOfMe(state1)) {
+            throw new Exception("failed");
+        }
+        if(!state1.isSubsetOfMe(state1)) {
+            throw new Exception("failed");
+        }
+        state1 = new BfsState(0, 0, 128, false);
+        state2 = new BfsState(2, 2, 1, false);
+        if(!state1.isSubsetOfMe(state2)) {
+            throw new Exception("failed");
+        }
+        if(!state2.isSubsetOfMe(state1)) {
+            throw new Exception("failed");
+        }
+        state1 = new BfsState(0, 0, 95, false);
+        state2 = new BfsState(4, 4, 86, false);
+        if(state1.isSubsetOfMe(state2)) {
+            throw new Exception("failed");
+        }
+        if(state2.isSubsetOfMe(state1)) {
+            throw new Exception("failed");
+        }
+        state1 = new BfsState(3, 5, 37, false);
+        state2 = new BfsState(3, 5, 255 - 37, false);
+        if(state1.isSubsetOfMe(state2)) {
+            throw new Exception("failed");
+        }
+        if(state2.isSubsetOfMe(state1)) {
+            throw new Exception("failed");
+        }
+        state1 = new BfsState(3, 5, 56, false);
+        state2 = new BfsState(3, 5, 57, false);
+        if(state1.isSubsetOfMe(state2)) {
             throw new Exception("failed");
         }
         if(!state2.isSubsetOfMe(state1)) {
